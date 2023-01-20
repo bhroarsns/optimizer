f37c1659993afa6270c1eb780e8c3f4f845a65a4
bhroarnsns2 <dixneufbhroarsns3mfcm@gmail.com>
2023-01-20 10:36:20 +0900

    optional logging

--- src/nelder_mead.rs
+++ src/nelder_mead.rs
@@ -1,6 +1,6 @@
 use super::Parameters;
 use super::distance;
-use std::io::{BufWriter, StderrLock, Write};
+use std::io::{BufWriter, Write};
 
 pub fn shift(target: &Parameters, center: &Parameters, coef: f64) -> Parameters {
     target.iter().zip(center.iter()).map(|(t, c)| {
@@ -8,9 +8,7 @@ pub fn shift(target: &Parameters, center: &Parameters, coef: f64) -> Parameters 
     }).collect()
 }
 
-pub fn optimize<T>(init: &Parameters, cost_function: T, delta: f64, epsilon: f64, lambda: f64, mut err: BufWriter<StderrLock>) -> Parameters
-where T: Fn(&Parameters) -> f64
-{
+pub fn optimize<T: Fn(&Parameters) -> f64, W: Write>(init: &Parameters, cost_function: T, delta: f64, epsilon: f64, lambda: f64, log: Option<BufWriter<W>>) -> Parameters {
     let dimension = init.len();
 
     let mut simplex: Vec<Parameters> = (0..=dimension).map(|i| {
@@ -27,52 +25,107 @@ where T: Fn(&Parameters) -> f64
         }
     }).collect();
 
-    loop {
-        simplex.sort_unstable_by(|a, b| cost_function(&a).partial_cmp(&cost_function(&b)).unwrap());
+    match log {
+        Some(mut out) => {
+            loop {
+                simplex.sort_unstable_by(|a, b| cost_function(&a).partial_cmp(&cost_function(&b)).unwrap());
 
-        let f_x_0 = cost_function(&simplex[0]);
-        let f_x_n = cost_function(&simplex[dimension]);
+                let f_x_0 = cost_function(&simplex[0]);
+                let f_x_n = cost_function(&simplex[dimension]);
 
-        writeln!(err, "{:?} {:?}", f_x_0, f_x_n).expect("");
+                writeln!(out, "{} {}", f_x_0, f_x_n).expect("");
 
-        if {
-            (f_x_n - f_x_0 < epsilon)
-            && (distance(&simplex[0], &simplex[dimension]) < delta)
-        } {
-            break;
-        }
+                if {
+                    (f_x_n - f_x_0 < epsilon)
+                    && (distance(&simplex[0], &simplex[dimension]) < delta)
+                } {
+                    break;
+                }
 
-        let x_o = simplex.iter().take(dimension).fold(
-            (0..dimension).map(|_| 0.0).collect(),
-            |tmp_sum: Parameters, vertex| {
-                tmp_sum.iter().zip(vertex.iter()).map(|(a, b)| a + b).collect()
-            }
-        ).into_iter().map(|sum| sum / dimension as f64).collect();
+                let x_o = simplex.iter().take(dimension).fold(
+                    (0..dimension).map(|_| 0.0).collect(),
+                    |tmp_sum: Parameters, vertex| {
+                        tmp_sum.iter().zip(vertex.iter()).map(|(a, b)| a + b).collect()
+                    }
+                ).into_iter().map(|sum| sum / dimension as f64).collect();
 
-        let x_r = shift(&simplex[dimension], &x_o, -1.0);
-        let f_x_r = cost_function(&x_r);
+                let x_r = shift(&simplex[dimension], &x_o, -1.0);
+                let f_x_r = cost_function(&x_r);
 
-        if f_x_r < f_x_0 {
-            let x_e = shift(&x_r, &x_o, 2.0);
-            if cost_function(&x_e) < f_x_r {
-                simplex.pop();
-                simplex.push(x_e);
-            } else {
-                simplex.pop();
-                simplex.push(x_r);
-            }
-        } else {
-            if f_x_r < cost_function(&simplex[dimension - 1]) {
-                simplex.pop();
-                simplex.push(x_r);
-            } else {
-                let x_c = shift(&simplex[dimension], &x_o, 0.5);
-                if cost_function(&x_c) < f_x_n {
-                    simplex.pop();
-                    simplex.push(x_c);
+                if f_x_r < f_x_0 {
+                    let x_e = shift(&x_r, &x_o, 2.0);
+                    if cost_function(&x_e) < f_x_r {
+                        simplex.pop();
+                        simplex.push(x_e);
+                    } else {
+                        simplex.pop();
+                        simplex.push(x_r);
+                    }
                 } else {
-                    for i in 1..=dimension {
-                        simplex[i] = shift(&simplex[i], &simplex[0], 0.5);
+                    if f_x_r < cost_function(&simplex[dimension - 1]) {
+                        simplex.pop();
+                        simplex.push(x_r);
+                    } else {
+                        let x_c = shift(&simplex[dimension], &x_o, 0.5);
+                        if cost_function(&x_c) < f_x_n {
+                            simplex.pop();
+                            simplex.push(x_c);
+                        } else {
+                            for i in 1..=dimension {
+                                simplex[i] = shift(&simplex[i], &simplex[0], 0.5);
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        None => {
+            loop {
+                simplex.sort_unstable_by(|a, b| cost_function(&a).partial_cmp(&cost_function(&b)).unwrap());
+
+                let f_x_0 = cost_function(&simplex[0]);
+                let f_x_n = cost_function(&simplex[dimension]);
+
+                if {
+                    (f_x_n - f_x_0 < epsilon)
+                    && (distance(&simplex[0], &simplex[dimension]) < delta)
+                } {
+                    break;
+                }
+
+                let x_o = simplex.iter().take(dimension).fold(
+                    (0..dimension).map(|_| 0.0).collect(),
+                    |tmp_sum: Parameters, vertex| {
+                        tmp_sum.iter().zip(vertex.iter()).map(|(a, b)| a + b).collect()
+                    }
+                ).into_iter().map(|sum| sum / dimension as f64).collect();
+
+                let x_r = shift(&simplex[dimension], &x_o, -1.0);
+                let f_x_r = cost_function(&x_r);
+
+                if f_x_r < f_x_0 {
+                    let x_e = shift(&x_r, &x_o, 2.0);
+                    if cost_function(&x_e) < f_x_r {
+                        simplex.pop();
+                        simplex.push(x_e);
+                    } else {
+                        simplex.pop();
+                        simplex.push(x_r);
+                    }
+                } else {
+                    if f_x_r < cost_function(&simplex[dimension - 1]) {
+                        simplex.pop();
+                        simplex.push(x_r);
+                    } else {
+                        let x_c = shift(&simplex[dimension], &x_o, 0.5);
+                        if cost_function(&x_c) < f_x_n {
+                            simplex.pop();
+                            simplex.push(x_c);
+                        } else {
+                            for i in 1..=dimension {
+                                simplex[i] = shift(&simplex[i], &simplex[0], 0.5);
+                            }
+                        }
                     }
                 }
             }
